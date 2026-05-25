22100fde4661b7dceb9768589fe9a85ff203d9dd
sone <sonesone.25@ichat.sp.edu.sg>
2026-05-26 01:26:23 +0800

    BMI

--- bmi.py
+++ bmi.py
@@ -6,10 +6,16 @@ def calculate_bmi(height,weight):
     print(f"calculated BMI:  {bmi:.2f}")
     if bmi < 18.5:
         print("Under Weight")
+        return -1
+
     elif (bmi >= 18.5) & (bmi <= 25.0):
         print("Normal Weight")
+        return 0
+
     else:
          print("Over weight")
-         
-calculate_bmi(weight =57, height=1.73)
+         return 1
 
+if __name__ == "__main__":
+    result = calculate_bmi(1.73, 57)
+    print("Return value =", result)
